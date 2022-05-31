a813898a9d2aad2e9abe63ca73e6c1d879f35fc1
p1atdev <shu_teiei@outlook.jp>
2022-05-31 18:31:32 +0900

    feat: add one test to client

--- test/client.test.ts
+++ test/client.test.ts
@@ -14,10 +14,28 @@ Deno.test("calil client: search libraries", async () => {
 
   const libraries = await client.searchLibrary({
     prefecture: "沖縄",
-    limit: 3,
+    limit: 2,
   });
 
-  assertEquals(libraries.length, 3);
+  console.dir(libraries, { depth: 4 });
+
+  assertEquals(libraries.length, 2);
 
   assertEquals(libraries[0].systemId, "Okinawa_Chatan");
+});
+
+Deno.test("calil client: search lending", async () => {
+  const client = new CalilClient({ appKey });
+
+  const lending = await client.searchLending({
+    isbn: "9784048923965", // キノの旅ＸＸ the Beautiful World
+    systemId: "Tokyo_NDL",
+  });
+
+  console.dir(lending, { depth: 8 });
+
+  assertEquals(
+    lending.books[0].libraries[0].reserveUrl,
+    "https://ndlonline.ndl.go.jp/#!/detail/R300000001-I027617183-00",
+  );
 });
